78c0defb3b53cdbe0c5935b9ab6926fb28059b22
Jim Blake <jeb228@cornell.edu>
2015-10-29 14:44:04 -0400

    Let the top-level file make the connection to triple_store_drivers and triple_store_controller.

--- lib/ld4l_ingesting.rb
+++ lib/ld4l_ingesting.rb
@@ -1,16 +1,24 @@
+$LOAD_PATH.unshift File.expand_path('../../../triple_store_drivers/lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../../triple_store_controller/lib', __FILE__)
+
 require 'benchmark'
 require 'fileutils'
 require 'find'
 require 'rdf'
 require 'rdf/ntriples'
 require 'tempfile'
-
 require "ld4l_ingesting/version"
 require "ld4l_ingesting/break_nt_files"
 require "ld4l_ingesting/convert_directory_tree"
 require "ld4l_ingesting/ingest_directory_tree"
 require "ld4l_ingesting/filter_ntriples"
 require "ld4l_ingesting/scan_directory_tree"
+
+module Kernel
+  def bogus(message)
+    puts(">>>>>>>>>>>>>BOGUS #{message}")
+  end
+end
 
 module Ld4lIngesting
   # You screwed up the calling sequence.
@@ -21,7 +29,4 @@ module Ld4lIngesting
   class UserInputError < StandardError
   end
 
-  def bogus(message)
-    puts(">>>>>>>>>>>>>BOGUS #{message}")
-  end
 end
